b704301ab81c3de823cddf15a0d7485b1c9dcb46
Nathan Bowling <nbowling142@gmail.com>
2026-06-24 17:15:32 -0400

    Blackjack v2

--- Day_10/blackjack.py
+++ Day_10/blackjack.py
@@ -4,7 +4,7 @@ from art import logo
 #functions
 def choose_card(deck):
     return random.choice(deck)
-
+    
 def compare_hands(hand1, hand2):
     if sum(hand2) <= 21:
         if sum(hand1) == sum(hand2):
@@ -16,15 +16,19 @@ def compare_hands(hand1, hand2):
     else:
         print("You win!")
 
-#deck of cards (unlimited)
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+def ace_check(hand):
+    while sum(hand) > 21 and 11 in hand:
+        hand.remove(11)
+        hand.append(1)
 
-#the game of blackjack
-def blackjack():
+#game logic
+def blackjack(deck):
 
-    #deal the initial hands
-    player_hand = [choose_card(cards) for card in range(2)]
-    computer_hand = [choose_card(cards)]
+    #deal the initial hands and check for aces
+    player_hand = [choose_card(deck) for card in range(2)]
+    computer_hand = [choose_card(deck) for card in range(2)]
+    ace_check(player_hand)
+    ace_check(computer_hand)
 
     #players turn, break once they pass
     while True:
@@ -34,7 +38,8 @@ def blackjack():
 
         hit = input("Type 'y' to get another card, type 'n' to pass: ").lower()
         if hit == 'y':
-            player_hand.append(choose_card(cards))
+            player_hand.append(choose_card(deck))
+            ace_check(player_hand)
             if sum(player_hand) > 21:
                 print(f"    Your Cards: {player_hand}, final score: {sum(player_hand)}")
                 print("    You went over 21. You lose!")
@@ -43,16 +48,19 @@ def blackjack():
             break
 
     #computers turn, breaks once it is >= the sum of player_hand
-    computer_hand.append(choose_card(cards))
-    while sum(computer_hand) < sum(player_hand):
-        computer_hand.append(choose_card(cards))
+    while sum(computer_hand) < sum(player_hand) or sum(computer_hand) < 17:
+        computer_hand.append(choose_card(deck))
+        ace_check(computer_hand)
 
     #compare the hands and print the results
     print(f"    Your final hand: {player_hand}, final score: {sum(player_hand)}")
     print(f"    Computer's final hand: {computer_hand}, final score: {sum(computer_hand)}")
     compare_hands(player_hand, computer_hand)
+
+#deck of cards (unlimited)
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     
 #start the game
 while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower() == 'y':
     print(logo)
-    blackjack()
+    blackjack(cards)
