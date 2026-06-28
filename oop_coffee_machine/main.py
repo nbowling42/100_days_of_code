ae708a2ddb6ebf2d56ab25a9ebe72bfa5cc9a1c1
Nathan Bowling <nbowling142@gmail.com>
2026-06-27 21:40:13 -0400

    oop_coffee_machine

---
+++ oop_coffee_machine/main.py
@@ -0,0 +1,21 @@
+from coffee_maker import CoffeeMaker
+from menu import Menu, MenuItem
+from money_machine import MoneyMachine
+
+coffee_machine = CoffeeMaker()
+money_machine = MoneyMachine()
+menu = Menu()
+
+while True:
+    choice = input(f"What would you like? ({menu.get_items()}): ").lower()
+
+    if choice == "off":
+        break
+    elif choice == "report":
+        coffee_machine.report()
+        money_machine.report()
+    else:
+        order = menu.find_drink(choice)
+        if coffee_machine.is_resource_sufficient(order):
+            if money_machine.make_payment(order.cost):
+                coffee_machine.make_coffee(order)
