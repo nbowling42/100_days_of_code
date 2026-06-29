cff820246e2afb0167538d8aa9a323105ee9fe59
Nathan Bowling <nbowling142@gmail.com>
2026-06-28 20:16:35 -0400

    quiz_game

---
+++ quiz_game/main.py
@@ -0,0 +1,13 @@
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+question_bank = []
+for q in question_data:
+    question_bank.append(Question(q["text"], q["answer"]))
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    answer = quiz.next_quetion()
+    quiz.check_answer(answer)
---
+++ quiz_game/quiz_brain.py
@@ -0,0 +1,26 @@
+class QuizBrain:
+
+    def __init__(self, question_list):
+        self.question_number = 0
+        self.score = 0
+        self.question_list = question_list
+
+
+    def next_quetion(self):
+        question = self.question_list[self.question_number].text
+        self.question_number += 1
+        return input(f"Q.{self.question_number}: {question} (True/False)?: ").title()
+        
+        
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
+    
+    def check_answer(self, answer):
+        index = self.question_number - 1
+        correct_answer = self.question_list[index].answer
+
+        if correct_answer == answer:
+            self.score += 1
+            print(f"Correct! Current Score: {self.score}")
+        else:
+            print(f"Incorrect. Current Score: {self.score}")
