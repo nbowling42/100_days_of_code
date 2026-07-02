c45031f56979669db217253f2e53d6102385c975
Nathan Bowling <nbowling142@gmail.com>
2026-07-02 18:01:45 -0400

    etch_a_sketch

--- etch_a_scketch/etch_a_sketch.py
+++ etch_a_scketch/etch_a_sketch.py
@@ -4,25 +4,58 @@ turtle = Turtle()
 screen = Screen()
 screen.listen()
 
-def move_forward():
-    turtle.forward(3)
+def forward_press():
+    key_strokes["w"] = True
+def forward_release():
+    key_strokes["w"] = False
 
-def move_backward():
-    turtle.backward(3)
+def backward_press():
+    key_strokes["s"] = True
+def backward_release():
+    key_strokes["s"] = False
 
-def turn_right():
-    turtle.right(3)
+def left_press():
+    key_strokes["a"] = True
+def left_release():
+    key_strokes["a"] = False
 
-def turn_left():
-    turtle.left(3)
+def right_press():
+    key_strokes["d"] = True
+def right_release():
+    key_strokes["d"] = False
 
 def clear_screen():
     screen.resetscreen()
 
+def move_turtle():
+        speed = 5
+        if key_strokes["w"]:
+            turtle.forward(speed)
+        if key_strokes["s"]:
+            turtle.backward(speed)
+        if key_strokes["a"]:
+            turtle.left(speed)
+        if key_strokes["d"]:
+            turtle.right(speed)
 
-screen.onkeypress(key="w", fun=move_forward)
-screen.onkeypress(key="s", fun=move_backward)
-screen.onkeypress(key="d", fun=turn_right)
-screen.onkeypress(key="a", fun=turn_left)
+        screen.ontimer(move_turtle, 10)
+
+key_strokes = {
+    "w": False,
+    "a": False,
+    "s": False,
+    "d": False,
+}
+
+screen.onkeypress(key="w", fun=forward_press)
+screen.onkeyrelease(key="w", fun=forward_release)
+screen.onkeypress(key="s", fun=backward_press)
+screen.onkeyrelease(key="s", fun=backward_release)
+screen.onkeypress(key="d", fun=right_press)
+screen.onkeyrelease(key="d", fun=right_release)
+screen.onkeypress(key="a", fun=left_press)
+screen.onkeyrelease(key="a", fun=left_release)
 screen.onkeyrelease(key="c", fun=clear_screen)
+
+move_turtle()
 screen.mainloop()
